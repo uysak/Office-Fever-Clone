83e84f84f82473a786b4d4ba81ab0ddec6baa9e5
Berkay Uysak <77589774+uysak@users.noreply.github.com>
2022-06-11 21:07:13 +0300

    Added: Player Movement

--- Assets/Scripts/Manager/PlayerMovementController.cs
+++ Assets/Scripts/Manager/PlayerMovementController.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovementManager : MonoBehaviour
+public class PlayerMovementController : MonoBehaviour
 {
     InputManager inputManager;
     AnimationController animationController;
+
+    private float movementSpeed;
     // Start is called before the first frame update
     void Start()
     {
+        movementSpeed = 0.02f;
         inputManager = this.GetComponent<InputManager>();
         animationController = GameObject.FindGameObjectWithTag("AnimationController").GetComponent<AnimationController>();
     }
@@ -19,8 +22,8 @@ public class PlayerMovementManager : MonoBehaviour
         if(inputManager.isTouchPress == true)
         {
             animationController.PlayerRunAnimation();
-            this.transform.position = Vector3.Lerp(this.transform.position, new Vector3(inputManager._Target.x * 200, 0f, inputManager._Target.y * 200), 0.01f * Time.deltaTime);
-            this.transform.rotation = Quaternion.LookRotation(Vector3.Lerp(this.transform.position, new Vector3(inputManager._Target.x * 200, 0f, inputManager._Target.y * 200), 400 * Time.deltaTime));
+            this.transform.position = Vector3.Lerp(this.transform.position, new Vector3(inputManager._Target.x * 200, 0f, inputManager._Target.y * 200), movementSpeed * Time.deltaTime);
+            this.transform.rotation = Quaternion.LookRotation(Vector3.Lerp(this.transform.position, new Vector3(inputManager._Target.x * 200, 0f, inputManager._Target.y * 200), 150 * Time.deltaTime));
         }
         else
         {
